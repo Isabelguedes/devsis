c1079466cb7d1d93bd1e3af63705b7ee0e21b9c3
Isabel Guedes <bell.ysa@hotmail.com>
2017-03-02 16:24:39 -0300

    Array e Matriz

---
+++ 01/exercicio/ExercicioDeRepasse/ExercicioDeRepasse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDeRepasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = "TV";
+            int productYear = 2012;
+            double productPrice = 279.99;
+
+            Console.WriteLine(productName);
+            Console.WriteLine(productYear);
+            Console.WriteLine(productPrice);
+
+            Console.ReadLine();
+         
+        }
+    }
+}
--- 01/tiposPrimitivos/tiposPrimitivos/Program.cs
+++ 01/tiposPrimitivos/tiposPrimitivos/Program.cs
@@ -8,27 +8,41 @@ namespace tiposPrimitivos
 {
     class Program
     {
-        enum Dias { domingo, segunda, terca, quarta, quinta, sexta, sabado };//Enum é uma estrutura de tipo de dados numerico, e não pode ficar dentro do metodo
+        enum Dias { domingo=100, segunda, terca, quarta, quinta, sexta, sabado };//Enum é uma estrutura de tipo de dados numerico, e não pode ficar dentro do metodo
 
         static void Main(string[] args)
         {
             //variaveis tipadas
             int numero = 3;
             float numero2 = 2f;
+            //Cast como fortalecimento de tipo e conversão explicita
             int resultado = numero + (int) numero2;
+            //String é um tipo de dado válido
             string texto = "Olá mundo";
 
             //bool é representado por true e false
             bool certidaoDeVivo = true;
 
-            Dias dia = Dias.quinta;
-
             //Console.WriteLine(numero);
             //Console.WriteLine(texto);
-           //Console.WriteLine(resultado);
-           // Console.WriteLine(texto+" Você esta vivo? {0}", certidaoDeVivo);
-           
-           
+            //Console.WriteLine(resultado);
+            // Console.WriteLine(texto+" Você esta vivo? {0}", certidaoDeVivo);
+
+            Dias dia = Dias.quinta;
+            //Console.WriteLine("A referencia numerica do dia : " + (int)dia);
+
+            //vetor Unidimensional
+            int[] num = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] arr = new int[10];
+
+            Console.WriteLine(num[5]);
+
+            //Matriz
+            int[,] mat = new int[5, 5];
+            mat[0, 1] = 1;
+            mat[0, 2] = 2;
+
+            Console.WriteLine("Valor da matriz na posição [0,1] :" + mat[0, 1]);
 
             Console.ReadLine();
 
