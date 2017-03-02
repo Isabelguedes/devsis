15755e6595c2d2d40c25981818828af2705b67a6
Isabel Guedes <bell.ysa@hotmail.com>
2017-03-02 15:22:15 -0300

    Dados Primitivos

---
+++ 01/tiposPrimitivos/tiposPrimitivos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiposPrimitivos
+{
+    class Program
+    {
+        enum Dias { domingo, segunda, terca, quarta, quinta, sexta, sabado };//Enum é uma estrutura de tipo de dados numerico, e não pode ficar dentro do metodo
+
+        static void Main(string[] args)
+        {
+            //variaveis tipadas
+            int numero = 3;
+            float numero2 = 2f;
+            int resultado = numero + (int) numero2;
+            string texto = "Olá mundo";
+
+            //bool é representado por true e false
+            bool certidaoDeVivo = true;
+
+            Dias dia = Dias.quinta;
+
+            //Console.WriteLine(numero);
+            //Console.WriteLine(texto);
+           // Console.WriteLine(resultado);
+           // Console.WriteLine(texto+" Você esta vivo? {0}", certidaoDeVivo);
+           
+           
+
+            Console.ReadLine();
+
+            //comentario em Cshar é escrito com //{tambem} 
+        }
+    }
+}
